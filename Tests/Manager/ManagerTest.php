a366ea423b6ec3cc1e965386947c9326ade3a737
Philippe Le Van (@plv) <philippe.levan@kitpages.fr>
2013-12-05 16:24:46 +0100

    more margin in unit testing

--- Tests/Manager/ManagerTest.php
+++ Tests/Manager/ManagerTest.php
@@ -19,31 +19,31 @@ class ManagerTest extends BundleOrmTestCase
 
     public function testBasicSemaphore()
     {
-        $manager = new Manager($this->getEntityManager()->getConnection(), 500000, 2000000);
+        $manager = new Manager($this->getEntityManager()->getConnection(), 500000, 4000000);
         $startTime = microtime(true);
         $manager->aquire("my_key");
         $duration = microtime(true) - $startTime;
-        $this->assertTrue($duration < 0.2);
+        $this->assertTrue($duration < 2);
 
         $manager->release("my_key");
 
         $manager->aquire("my_key");
         $duration2 = microtime(true) - $startTime;
-        $this->assertTrue($duration2 < 0.5);
+        $this->assertTrue($duration2 < 3);
 
     }
 
     public function testExpiration()
     {
-        $manager = new Manager($this->getEntityManager()->getConnection(), 500000, 2000000);
+        $manager = new Manager($this->getEntityManager()->getConnection(), 500000, 4000000);
         $startTime = microtime(true);
         $manager->aquire("my_key");
         $duration = microtime(true) - $startTime;
-        $this->assertTrue($duration < 0.2);
+        $this->assertTrue($duration < 2);
 
         $manager->aquire("my_key");
         $duration2 = microtime(true) - $startTime;
-        $this->assertTrue($duration2 > 2);
+        $this->assertTrue($duration2 > 4);
 
     }
 }
